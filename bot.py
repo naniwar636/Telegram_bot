d91b7b87c96fed3375ae6b66716a9b7999c87d57
naniwar636 <naniwar636@gmail.com>
2025-03-19 18:36:47 -0400

    Corrección: Definir main() antes de ejecutarla

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 from telegram import Update, Bot
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler
 import asyncio
 
 # Reemplaza con tu token de bot
-BOT_TOKEN = "7893913594:AAGRkpGC_SAcik_RyStLFPT6xHvABBz7PbE"
+BOT_TOKEN = "7893913594:AAGRkpGC_SAcik_RyStLFPT6xHvABBz7PDE"
 CHAT_ID = "5370010407"
 
 # Función para responder al comando /start
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start(update: Update, context):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="El bot de Fetcher está activo")
 
 # Configurar el bot
@@ -15,12 +15,14 @@ app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
 # Ejecutar el bot
-import asyncio
+async def main():
+    await app.run_polling()
 
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(main())
+
 
 
 
